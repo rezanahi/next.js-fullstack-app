242f694812e7cd213103cdcb41cb0f8f00e6a184
rezanahi <raad.thunder1@gmail.com>
2024-09-29 17:49:08 +0330

    Create Not-Found layout

---
+++ src/app/not-found.jsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+function NotFound () {
+    return(
+        <>
+            <div>
+                <h2>Not Found</h2>
+                <p>Sorry, the page you are looking for does not exist</p>
+                <Link href='/'>Return Home</Link>
+            </div>
+        </>
+    )
+}
+
+
+export default NotFound
