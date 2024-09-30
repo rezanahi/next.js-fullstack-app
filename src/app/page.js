0c7f3da117d5586a276c8a786f0b96f7a246d4c9
rezanahi <raad.thunder1@gmail.com>
2024-09-30 17:14:01 +0330

    Home Page Design

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,24 @@
 // import Image from "next/image";
-// import styles from "./page.module.css";
+import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      Hello This is a Home Page
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughts Agency.</h1>
+        <p className={styles.desc}>Lorem15</p>
+          <div className={styles.btns}>
+              <button className={styles.btn}>Learn More</button>
+              <button className={styles.btn}>Contact</button>
+          </div>
+          <div className={styles.brands}>
+              <Image src='/brands.png' alt='' fill className={styles.brandImg}/>
+          </div>
+      </div>
+        <div className={styles.imageContainer}>
+            <Image src='/hero.gif' alt='' fill className={styles.heroImg}/>
+        </div>
     </div>
   );
 }
