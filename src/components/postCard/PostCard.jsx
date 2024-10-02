4890a2eb3a2936b4e64fd1114bae0c75fa7be83d
rezanahi <raad.thunder1@gmail.com>
2024-10-02 20:59:33 +0330

    Complete PostCard component

--- src/components/postCard/PostCard.jsx
+++ src/components/postCard/PostCard.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import './PostCard.module.css'
+import styles from './PostCard.module.css'
+import Image from "next/image";
+import Link from "next/link";
 
 function PostCard() {
 
 
     return(
         <>
-            <div>PostCard</div>
+            <div className={styles.container}>
+                <div className={styles.top}>
+                    <div className={styles.imgContainer}>
+                        <Image src={'/post1.jpg'} alt='' fill className={styles.img}/>
+                    </div>
+                    <span className={styles.date}>01.01.2024</span>
+                </div>
+                <div className={styles.bottom}>
+                    <h1 className={styles.title}>Title</h1>
+                    <p className={styles.desc}>Desc</p>
+                    <Link className={styles.link} href='/blog/post'>READ MORE</Link>
+                </div>
+            </div>
         </>
     )
 }
