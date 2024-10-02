b170e423d769b9870816272ef61354d3901974cd
rezanahi <raad.thunder1@gmail.com>
2024-10-02 20:25:18 +0330

    contact page design

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,30 @@
+import styles from './contact.module.css'
+import Image from "next/image";
+
 function Contact () {
 
     return (
         <>
-            <p>Contact</p>
+            <div className={styles.container}>
+                <div className={styles.imgContainer}>
+                    <Image className={styles.img} src='/contact.png' alt='' fill/>
+                </div>
+                <div className={styles.formContainer}>
+                    <form action='' className={styles.form}>
+                        <input type='text' placeholder='Name and Surname' />
+                        <input type='email' placeholder='Email Address' />
+                        <input type='text' placeholder='Phone Number (Optional)' />
+                        <textarea
+                            name=''
+                            id=''
+                            cols='30'
+                            rows='10'
+                            placeholder='Message'>
+                        </textarea>
+                        <button>Send</button>
+                    </form>
+                </div>
+            </div>
         </>
     )
 }
