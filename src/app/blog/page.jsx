fc57de9f442702edcafa0d801ec0ff962ebc5359
rezanahi <raad.thunder1@gmail.com>
2024-10-02 20:59:55 +0330

    Complete blog page design

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,15 +1,26 @@
-import './page.module.css'
+import './blog.module.css'
 import '@/components/postCard/PostCard'
 import PostCard from "@/components/postCard/PostCard";
+import styles from './blog.module.css'
 
 function Blog () {
 
     return (
         <>
-            <PostCard></PostCard>
-            <PostCard></PostCard>
-            <PostCard></PostCard>
-            <PostCard></PostCard>
+            <div className={styles.container}>
+                <div className={styles.post}>
+                    <PostCard></PostCard>
+                </div>
+                <div className={styles.post}>
+                    <PostCard></PostCard>
+                </div>
+                <div className={styles.post}>
+                    <PostCard></PostCard>
+                </div>
+                <div className={styles.post}>
+                    <PostCard></PostCard>
+                </div>
+            </div>
         </>
     )
 }
