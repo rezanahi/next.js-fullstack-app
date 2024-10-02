af7cf8102883317d626a080cff6d3ae4acd1d7dd
rezanahi <raad.thunder1@gmail.com>
2024-10-02 21:06:57 +0330

    Create jsx - singlepost page design

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,9 +1,33 @@
+import styles from './singlePost.module.css'
+import Image from "next/image";
+
 function Post () {
 
 
     return (
         <>
-            <p>Post</p>
+            <div className={styles.container}>
+                <div className={styles.imgContainer}>
+                    <Image src={'/post2.jpg'} alt='' className={styles.img} fill />
+                </div>
+                <div className={styles.textContainer}>
+                    <h1 className={styles.title}>Title</h1>
+                    <div className={styles.detail}>
+                        <Image src={'/post3.jpg'} alt='' className={styles.avatar} fill/>
+                        <div className={styles.detailText}>
+                            <span className={styles.detailTitle}>Author</span>
+                            <span className={styles.detailValue}>Terry Jefferson</span>
+                        </div>
+                        <div className={styles.detailText}>
+                            <span className={styles.detailTitle}>Published</span>
+                            <span className={styles.detailValue}>01.01.2024</span>
+                        </div>
+                    </div>
+                    <div className={styles.content}>
+                        lotrem lorem lorem lorem
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
