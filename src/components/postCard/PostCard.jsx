19f7da268f5b003411d904704021c40890cdc8b1
rezanahi <raad.thunder1@gmail.com>
2024-10-14 00:00:47 +0330

    correction postCard

--- src/components/postCard/PostCard.jsx
+++ src/components/postCard/PostCard.jsx
@@ -3,22 +3,21 @@ import styles from './PostCard.module.css'
 import Image from "next/image";
 import Link from "next/link";
 
-function PostCard(props) {
-    console.log(props.postData)
+function PostCard({postData}) {
 
     return(
         <>
             <div className={styles.container}>
                 <div className={styles.top}>
                     <div className={styles.imgContainer}>
-                        <Image src={'/post1.jpg'} alt='' fill className={styles.img}/>
+                        <Image src={postData.img ? postData.img : '/post1.jpg'} alt='' fill className={styles.img}/>
                     </div>
                     <span className={styles.date}>01.01.2024</span>
                 </div>
                 <div className={styles.bottom}>
-                    <h1 className={styles.title}>props.postData.title</h1>
-                    <p className={styles.desc}>props.postData.body</p>
-                    <Link className={styles.link} href='/blog/post'>Read More</Link>
+                    <h1 className={styles.title}>{postData.title}</h1>
+                    <p className={styles.desc}>{postData.desc}</p>
+                    <Link className={styles.link} href={`/blog/${postData.slug}`}>Read More</Link>
                 </div>
             </div>
         </>
