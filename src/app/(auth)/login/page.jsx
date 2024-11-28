cb20d9f739bc9f417087a1783394b5804480db0e
rezanahi <raad.thunder1@gmail.com>
2024-11-28 23:38:15 +0330

    add login with credentials

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,11 +1,16 @@
 import {signIn, auth, signOut} from "../../../../lib/auth";
-import {handleGithubLogin, handleGithubLogout} from '../../../../lib/actions'
+import {handleGithubLogin, handleGithubLogout, login} from '../../../../lib/actions'
 async function Login () {
 
     return(
         <>
             <form action={handleGithubLogin}>
                 <button>Login with Github</button>
+            </form>
+            <form action={login}>
+                <input type='text' placeholder='username' name='username'/>
+                <input type='password' placeholder='password' name='password'/>
+                <button>Login</button>
             </form>
         </>
     )
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,9 +1,17 @@
+import {register} from "../../../../lib/actions";
+
 function Register () {
 
 
     return (
         <>
-            <p>Register</p>
+            <form action={register}>
+                <input type='text' name='username' placeholder='username'/>
+                <input type='email' name='email' placeholder='email'/>
+                <input type='password' name='password' placeholder='password'/>
+                <input type='password' name='password2' placeholder='password repeat'/>
+                <button>Register</button>
+            </form>
         </>
     )
 }
